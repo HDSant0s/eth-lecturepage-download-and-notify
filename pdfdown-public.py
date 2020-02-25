af531e688004cef2137d4884ee894ae2f35812a0
Sant0s <diego.delossantos12@gmail.com>
2020-02-25 10:29:27 +0100

    Initial fork commit thing

--- pdfdown-public.py
+++ pdfdown-public.py
@@ -5,54 +5,69 @@ import os
 import requests
 import ssl
 from urllib.parse import urlparse
+import urllib.parse
+import getpass
 
-api = 'https://api.telegram.org/bot<apiKey>/sendMessage?chat_id=<chat-id>&text='
-subject = 'subject name'
+URLS = {"Parallel Programming" : "https://spcl.inf.ethz.ch/Teaching/2020-pp/",
+        "Algorithmen und Wahrscheinlichkeit" : "https://www.cadmo.ethz.ch/education/lectures/FS20/AW/index.html",
+        "Analysis I" : "https://metaphor.ethz.ch/x/2020/fs/401-0212-16L/",
+        "Analysis I/Battilana" : "https://battilana.uk/analysis-i-fs20/"}
 
-def download(url):
-    response = requests.get(url)
-    if(response.status_code == 200):
-        filename = url[url.rfind("/")+1:]
-        with open(filename, 'wb') as fd:
-            fd.write(response.content)
-            # add file to the list of downloaded objects
-            links.add(url)
-            fd.close
-            print(filename)
-            payload = {'value1': filename, 'value2': subject}
-            requests.post(
-                'https://maker.ifttt.com/trigger/telegram/with/key/<ifttt-key>', params=payload)
-            requestStr = api + 'New File online for ' + subject + ': ' + filename + ' ' + url
-            requests.post(requestStr)
+# URLS = ["https://spcl.inf.ethz.ch/Teaching/2020-pp/",
+#         "https://www.cadmo.ethz.ch/education/lectures/FS20/AW/index.html",
+#         "https://metaphor.ethz.ch/x/2020/fs/401-0212-16L/"]
+
+usedLinks = []
+
+auth = None
+
+def download(links):
+    for dir in links:
+        for link in links[dir] :
+            response = requests.get(link, allow_redirects=True, auth=auth)
+            #print("Url: {}, Respnse:{}".format(url, response))
+            if(response.status_code == 200):
+                filename = dir + "/" + link[link.rfind("/")+1:].replace("%20", " ")
+                os.makedirs(os.path.dirname(filename), exist_ok=True)
+                with open(filename, 'wb') as fd:
+                    fd.write(response.content)
+                    # add file to the list of downloaded objects
+                    usedLinks.append(link)
+                    fd.close
+                    print(filename)
+            else:
+                print("Url: {}, Response: {}".format(link, response))
 
 
-def checkUrl(link, site):
+def checkLink(link):
     if (link[-4:] == ".pdf") or (link[-4:] == ".zip"):
-        if site[-10:] == 'index.html':
-            site = site[:-10]
-            link = site + link
-            print('link: ', link)
-            download(link)
-        if not (link in links):
-            download(link)
+        if not (link in usedLinks):
+            return True
 
+def getLinks(sites):
+    links = {}
 
-def getLinks(site):
-    http = httplib2.Http()
-    status, response = http.request(site)
-    print("res: ", status)
+    for site in sites.items():
+        url = site[1]
+        dir = site[0]
 
-    for link in BeautifulSoup(response, "html.parser", parse_only=SoupStrainer('a')):
-        # print("link: ", link)
-        if link.has_attr('href'):
-            checkUrl(link['href'], site)
+        response = requests.get(url, auth=auth)
 
+        links[dir] = []
 
-links = {1, 2}
+        for link in BeautifulSoup(response.text, "html.parser", parse_only=SoupStrainer('a')):
+            if link.has_attr('href'):
+                href = link['href'].replace(" ", "%20")
+                if not (href[:4] == "http"):
+                    href = urllib.parse.urljoin(url, href)
+                if checkLink(href):
+                    links[dir].append(href)
+
+    return links
 
 try:
     dmp = open(__file__ + ".dmp", "rb")
-    links = pickle.load(dmp)
+    usedLinks = pickle.load(dmp)
 except EOFError:
     dmp.close()
     pass
@@ -61,8 +76,15 @@ except FileNotFoundError:
 else:
     dmp.close()
 
-getLinks("<website-url>")
+
+userName = input("Username: ")
+if (userName != ""):
+    userPassword = getpass.getpass()
+    auth = (userName, userPassword)
+
+links = getLinks(URLS)
+download(links)
 
 dmp = open(__file__ + ".dmp", "wb")
-pickle.dump(links, dmp)
+pickle.dump(usedLinks, dmp)
 dmp.close()
